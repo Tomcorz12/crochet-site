72f1c38ebf71d3e6bf4785d183bad3a15e840ef7
Tomas Gonzalez <tomcorz12@gmail.com>
2024-11-14 19:56:45 -0500

    More changes made

---
+++ crochet_site/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.TextField(max_length = 200, verbose_name = 'Nombre')
+    description = models.TextField(max_length = 500, verbose_name = 'Descripción')
+    price = models.DecimalField(max_digits = 10, decimal_places = 2, verbose_name = 'Precio') #Nos dejará agregar máximo dos decimales
+    available = models.BooleanField(default = True, verbose_name = 'Disponible')
+    photo = models.ImageField(upload_to = 'Logos', null = True, blank = True, verbose_name = 'Foto')
+    
+    def __str__(self):
+        return self.name
